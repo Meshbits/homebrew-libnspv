e2b14bdd5bd6b908e9eeb23ce6e0f69deff2a82e
ns408 <nsrea408@gmail.com>
2019-08-15 20:00:17 +1000

    update release from jl777's repo

--- libnspv.rb
+++ libnspv.rb
@@ -3,9 +3,9 @@ class Libnspv < Formula
   Smart Chains, including the KMD main chain data structures and interacting
   with the p2p network"
   homepage "https://docs.komodoplatform.com/nSPV"
-  url "https://github.com/jl777/libnspv/archive/0.1.tar.gz"
-  version "0.1"
-  sha256 ""
+  url "https://github.com/jl777/libnspv/archive/v0.1.tar.gz"
+  version "v0.1"
+  sha256 "ca295bfc0d65aa5e58e8131fe0ef4b31cd928a17df2f1287eb2dfee62218f5e6"
 
   # depends_on "cmake" => :build
   depends_on "libsodium"
